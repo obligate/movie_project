315d38499754619894eef48b82248165b4e3e0ca
peter <hly_cf@126.com>
2018-08-22 14:44:34 +0800

    init

---
+++ app.py
@@ -0,0 +1,14 @@
+# coding: utf8
+# Author: Peter
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    return "<h1 style='color:red'>hello world</h1>"
+
+
+if __name__ == '__main__':
+    app.run()
